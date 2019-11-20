db7ff5d3731a0bb27725f64672040c774a749509
eric <skynocover@Gmail.com>
2019-11-20 21:22:57 +0800

    1.1.0

--- README.md
+++ README.md
@@ -9,15 +9,22 @@
 
 
 ## How to use
-> 請直接複製,接著按照下面的步驟修改
+> 請直接clone,接著按照下面的步驟修改
+
+git clone https://github.com/skynocover/GolangUI
 
 ### 基本設定
-- Inputtitle表整個視窗的標題
-- Inputlabel表在輸入格旁的標題
-- Inputbotton表按鈕的名稱
-- Makeview為視窗的大小
+> 所有的設定都在var內
 
 ![](examples/Howtouse.png)
+
+```go
+viewtitle  //整個視窗的標題
+inputlabel //在輸入框左側的文字
+bottonname //於按鈕上顯示的文字
+vieweight //視窗的寬度
+viewheight //視窗的高度
+```
 
 ### 程式執行
 
@@ -25,13 +32,30 @@
 
 ![](examples/Func.png)
 
-- input 是輸入的數值  
-- `document.querySelector('.done').innerText =` 則會輸出到按鈕下面
+- 按下按鈕後會將輸入格內的文字變成字串丟入input內
+- 直接使用input就可以進行運算
+- `document.querySelector('.done').innerText =` 則會將字串輸出到按鈕下面的位置
+
+## Example
+
+[範例](https://github.com/skynocover/Edown)
 
 
 # Vision
 
-## 1.0.0 (2019-11-14)
+## [1.1.0] 2019-11-20
+### Changed
+- 修改宣告方式,把ui宣告成全域,讓輸出的方式不只在main()內
+> 詳情可以參考[Edown](https://github.com/skynocover/Edown) [1.3.0]版
+
+## [1.0.1] 2019-11-19
+
+### Changed
+- 修改宣告方式
+
+----
+
+## [1.0.0] 2019-11-14
 
 最初版本釋出
 
--- UI.go
+++ UI.go
@@ -7,24 +7,16 @@ import (
 )
 
 var (
-	viewtitle = "Title"
-
-	
-	inputlabel     = "輸入"
-	bottonname        = "執行"
+	viewtitle      = "視窗標題"
+	inputlabel     = "標題"
+	bottonname     = "按鈕名稱"
 	vieweight  int = 520
 	viewheight int = 320
-	
+
+	ui, err = lorca.New("", "", vieweight, viewheight)
 )
 
 func main() {
-
-	Inputtitle("視窗標題")	
-	Inputlabel("標題")
-	Inputbotton("按鈕名稱")
-	Makeview(500, 400)
-
-	ui, err := lorca.New("", "", vieweight, viewheight)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,29 +31,11 @@ func main() {
 	<-ui.Done()
 }
 
-//out Call
-
-func Inputtitle(title string){
-	viewtitle = title
-}
-
-func Makeview(width int, height int) {
-	vieweight = width
-	viewheight = height
-}
-
-func Inputlabel(label string) {
-	inputlabel = label
-}
-func Inputbotton(botton string) {
-	bottonname = botton
-}
-
 //in call
 func buildhtml() string {
 	htmlstr := `<html>
 	<head>
-	<title>`+viewtitle+`</title>
+	<title>` + viewtitle + `</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	</head>
@@ -73,11 +47,11 @@ func buildhtml() string {
 	</html> 
 	`
 
-	return htmlstr+
-	 `
+	return htmlstr +
+		`
 	 	<label for="name" style="font-size:16px;">` + inputlabel + `</label>
 		<input id="input" type="text" value=""  SIZE=40  height="35" style="font-size:16px;">
 		</div>
 		<input type="button" onclick="execute(document.querySelector('#input').value)" style="width:100px;height:30px;font-size:16px;" value="` + bottonname + `">
-	`+ htmlend
+	` + htmlend
 }
